d4fe88bcb11b8a80d7d9c68f371d216eca2e3c1f
8WEvT <8WEvT@users.noreply.github.com>
2020-11-09 16:24:35 +0300

    ...

--- steps/4905/Main.java
+++ steps/4905/Main.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeoutException;
 
 @SuppressWarnings("CommentedOutCode")
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         /* Create a callable */
         final Callable<Integer> generator = () -> {
             TimeUnit.SECONDS.sleep(5L);
